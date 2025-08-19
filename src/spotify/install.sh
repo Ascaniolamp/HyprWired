cdd4c8a08d87a844ec6d373950672b892edea917
Ascaniolamp <ascaniolamp@gmail.com>
2025-08-19 20:08:09 +0200

    Reordered spotify/spicetify install lines

--- src/spotify/install.sh
+++ src/spotify/install.sh
@@ -5,6 +5,7 @@ GITSRC=$(dirname "$0")/src
 downdependencies "$GITSRC/pacpkgs.lst" "$GITSRC/aurpkgs.lst"
 
 echo "prefs_path = $HOME/.config/spotify/prefs" >> "$GITSRC/config-xpui.ini"
+echo "spotify_path = $HOME/.local/share/spotify-launcher/install/usr/share/spotify/" >> "$GITSRC/config-xpui.ini"
 substitute "$BAKORDEL" "$HOME/.config/spicetify/Themes/Hyprlain" "$GITSRC/Hyprlain"
 substitute "$BAKORDEL" "$HOME/.config/spicetify/config-xpui.ini" "$GITSRC/config-xpui.ini"
 
@@ -12,10 +13,10 @@ mkdir -p "$HOME/.config/spotify"
 touch "$HOME/.config/spotify/prefs"
 
 (curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh) || true
-spicetify
-spicetify backup apply enable-devtools
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
+spicetify
+spicetify backup apply enable-devtools
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 spicetify apply
 
