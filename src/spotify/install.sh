5ab46483893779467e6e4c0de50ea5d7e5987ef9
Ascaniolamp <ascaniolamp@gmail.com>
2025-08-19 19:41:15 +0200

    Spicetify reordered installation order to fix marketplace installation

--- src/spotify/install.sh
+++ src/spotify/install.sh
@@ -3,15 +3,14 @@ source $(dirname "$0")/../helper.sh
 GITSRC=$(dirname "$0")/src
 
 downdependencies "$GITSRC/pacpkgs.lst" "$GITSRC/aurpkgs.lst"
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 
-spicetify
-spicetify backup apply enable-devtools
-
-substitute "$BAKORDEL" "$HOME/.config/spicetify/Themes/Hyprlain" "$GITSRC/Hyprlain"
 echo "prefs_path = $HOME/.config/spotify/prefs" >> "$GITSRC/config-xpui.ini"
+substitute "$BAKORDEL" "$HOME/.config/spicetify/Themes/Hyprlain" "$GITSRC/Hyprlain"
 substitute "$BAKORDEL" "$HOME/.config/spicetify/config-xpui.ini" "$GITSRC/config-xpui.ini"
 
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
+spicetify
+spicetify backup apply enable-devtools
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 spicetify apply
 
